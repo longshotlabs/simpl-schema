30bda563929361a6d820635077c2c908a5dc920b
Victor Parpoil <vparpoil@gmail.com>
2018-06-15 12:46:09 +0200

    avoid returning a defaultValue when using the operator $pull

--- package/lib/SimpleSchema.js
+++ package/lib/SimpleSchema.js
@@ -894,6 +894,10 @@ function getDefaultAutoValueFunction(defaultValue) {
     if (this.isSet) return;
     if (this.operator === null) return defaultValue;
 
+    // Handle the case when pulling an object from an array the object contains a field
+    // which has a defaultValue. We don't wan't the default value to be returned in this case
+    if (this.operator === '$pull') return;
+
     // Handle the case where we are $pushing an object into an array of objects and we
     // want any fields missing from that object to be added if they have default values
     if (this.operator === '$push') return defaultValue;
